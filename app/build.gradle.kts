213ea6bc932db36798a07f8181648df513b50142
Matt Ott <matthew.w.ott@gmail.com>
2024-12-27 19:24:45 -0800

    Fixed kotlin composeOptions to kotlin version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,7 +42,7 @@ android {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.1"
+        kotlinCompilerExtensionVersion = libs.versions.kotlin.get()
     }
     packaging {
         resources {
